2c5677bd68e2a68d0e54ac220f39bebeb743f363
USER <stanmburu254@gmail.com>
2021-09-07 16:58:09 +0300

    task_tracker react crash course

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from 'react';
+import AddTask from "./components/AddTask";
 function App() {
+  const [showAddTask,setShowAddTask]=useState(false)
+  const [tasks, setTasks] = useState(
+    [
+        {
+           id:1,
+           text:'Doctors appointment',
+           day: 'feb 2021',
+           reminder: true,
+        },
+        {
+            id:2,
+            text:'Meeting at school',
+            day:'20th june',
+            reminder:true,
+    
+        },
+        {
+            id:3,
+            text:'Meeting at school',
+            day:'20th june',
+            reminder:true,
+    
+        }
+    ]
+)
+const addTask =(task)=>{
+  const id = Math.floor(Math.random() * 10000)+1
+  const newTask ={id, ...task}
+  setTasks([...tasks, newTask])
+}
+const deleteTask=(id) =>{
+  setTasks(tasks.filter((task) => task.id !==id))
+}
+//  toggle reminder
+const toggleReminder = (id) =>{
+  setTasks(
+    tasks.map((task) =>
+     task.id ===id?
+      {...task, reminder:
+        !task.reminder} : task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+ <Header onAdd={()=>setShowAddTask(!showAddTask)}
+  showAdd={showAddTask} 
+ />
+ {showAddTask && < AddTask  onAdd={addTask}/>}
+ {tasks.length>0 ?(
+ <Tasks tasks={tasks} 
+ onDelete={deleteTask}
+ onToggle = {toggleReminder}
+ />):( 'No tasks to show')}
     </div>
   );
 }
